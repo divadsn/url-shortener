72ddbfd56b827ea04faba3fddff8c91d943afb3e
David Sn <divad.nnamtdeis@gmail.com>
2018-05-24 12:43:35 +0200

    Hello World!

--- shortener/__init__.py
+++ shortener/__init__.py
@@ -1,2 +1,17 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+import re
+from os import path
+
+from flask import (Flask, Response, jsonify, redirect, render_template,
+    request, send_from_directory, url_for, session)
+
+from shortener import default_config
+
+app = Flask(__name__)
+app.config.from_object(default_config)
+if path.exists('config.py'):
+    import config
+    app.config.from_object(config)
+
+@app.route('/hello')
+def helloWorld():
+    return "Hello World!"
